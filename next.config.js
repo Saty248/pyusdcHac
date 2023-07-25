bf79e3bfcd32f8138d5ff210d666c1e0e03c0769
Ugwu Hilary <ugwuhilary017@gmail.com>
2023-07-25 10:59:48 +0100

    update

--- next.config.js
+++ next.config.js
@@ -11,14 +11,15 @@ const nextConfig = (phase) => {
         PORT: 3306
       }
     }
-  }
-  return {
-    env: {
-      USER: "sky_claim_dev",
-      DATABASE: "sky_claim_dev",
-      PASSWORD: "Pw2dv8i8g4hmkj",
-      HOST: "mysql-17204-0.cloudclusters.net",
-      PORT: 17204 
+  } else {
+    return {
+      env: {
+        USER: "sky_claim_dev",
+        DATABASE: "sky_claim_dev",
+        // PASSWORD: "Pw2dv8i8g4hmkOj",
+        HOST: "mysql-17204-0.cloudclusters.net",
+        PORT: 17204 
+      }
     }
   }
 }
