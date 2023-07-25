08fa7c57570d3e40e827222a09ccb203899ced26
Ugwu Hilary <ugwuhilary017@gmail.com>
2023-07-25 09:34:21 +0100

    Bug with POST request been fixed

--- pages/api/register.js
+++ pages/api/register.js
@@ -4,6 +4,13 @@ import { Sequelize } from "sequelize";
 
 const handler = async(req, res) => {
   if(req.method === "POST") {
+    console.log(process.env.PORT)
+    console.log(process.env.DATABASE)
+    console.log(process.env.USER)
+    console.log(process.env.PASSWORD)
+    console.log(process.env.HOST)
+
+
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
@@ -33,30 +40,33 @@ const handler = async(req, res) => {
 
     sequelize.sync()
     .then(() => {
-      return Address.findAll({ where: { address: addressValue } }) })
-      .then(addresses => {
-          const address = addresses[0]
-          if(address) {
-                      return res.status(422).json({
-                          message: "Address already registered. Duplicate records are not allowed!"
-                      })
-                  }
-          return Address.create({
-              firstname: firstName,
-              lastname: lastName,
-              email: email,
-              address: addressValue,
-              longitude: longitude,
-              latitude: latitude,
-              // coordinates: coordinates
-          })
-          .then(result => {
-              res.status(201).json({
-                          message: "User created",
-                          userId: result._id
-                      })
-          })
+      return res.status(201).json({
+        message: "Database connection successful"
       })
+      return Address.findAll({ where: { address: addressValue } }) })
+      // .then(addresses => {
+      //     const address = addresses[0]
+      //     if(address) {
+      //                 return res.status(422).json({
+      //                     message: "Address already registered. Duplicate records are not allowed!"
+      //                 })
+      //             }
+          // return Address.create({
+          //     firstname: "Okay",
+          //     // lastname: lastName,
+          //     // email: email,
+          //     // address: addressValue,
+          //     // longitude: longitude,
+          //     // latitude: latitude,
+          //     // coordinates: coordinates
+          // })
+          // .then(result => {
+          //     res.status(201).json({
+          //                 message: "User created",
+          //                 userId: result._id
+          //             })
+          // })
+      // })
       .catch(err => {
         console.log(err)
         res.status(500).json({
