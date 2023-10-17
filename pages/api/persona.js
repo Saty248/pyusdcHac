0ce4620178c7769eefacdb927f892065c55e384c
sobhakhadka98 <42111394+sobhakhadka98@users.noreply.github.com>
2023-10-17 22:28:15 +0530

    persona integrated (#17)
    * persona integrated
    
    * started the integration of solana RPC for fetching balance and transaction history
    
    * persona integrated
    
    * updated persona client ids
    
    ---------
    
    Co-authored-by: Ugwu Hilary <ugwuhilary017@gmail.com>

---
+++ pages/api/persona.js
@@ -0,0 +1,53 @@
+import { buffer } from "micro";
+const { createHmac, timingSafeEqual } = await import('node:crypto'); 
+
+export const config = {
+    api: {
+        bodyParser: false,
+    }
+}
+
+const handler = async (req, res) => {
+    if(req.method === "POST") {
+        const rawBody = await buffer(req);
+        console.log("referenceId => ", JSON.parse(rawBody).data.attributes.payload.data.attributes['reference-id']);
+        console.log("event => ",JSON.parse(rawBody).data.attributes.name);
+        console.log("signature => ", req.headers['persona-signature']);
+
+        try {
+            const data = JSON.parse(rawBody).data.attributes;
+            const reqBody = {
+              signature: req.headers['persona-signature'],
+              userId: Number(data.payload.data.attributes['reference-id']),
+              event: data.name,
+              rawBody: rawBody
+            }
+            // console.log(reqBody);
+            const fetchOptions = {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                api_key: process.env.FRONTEND_API_KEY,
+              },
+              body: JSON.stringify(reqBody)
+            };
+            const fetchRes = await fetch(
+              `${process.env.SERVER_URL}/services/persona`,
+              fetchOptions
+            );
+            const resData = await fetchRes.json();
+
+              if (resData?.data && resData?.data?.statusCode >= 400) {
+                throw new Error(resData.data.message);
+              }
+          } catch (err) {
+            let message = "Unknown Error";
+            if (err instanceof Error) message = err.message;
+            res.status(400).send(`Webhook Error: ${message}`);
+            return;
+          }
+          res.json({ received: true });
+    }
+}
+
+export default handler;
