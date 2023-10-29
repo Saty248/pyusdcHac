d7f396bca4567a57944f634a7a361137cd419ec6
JuriK <j.koniwszczynski@gmail.com>
2023-10-29 14:05:39 +0100

    Dev (#24)
    deployment chenges

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps : [{
+    name   : "sky.trade.frontend",
+    script: 'node_modules/next/dist/bin/next',
+    autorestart: true,
+    watch: false,
+    env_dev: {
+      NODE_ENV: "dev"
+    },
+    env_prod: {
+      NODE_ENV: "prod"
+    }
+  }],
+  deploy: {
+    prod: {
+      key: "~/.ssh/skytrade_deploy_key.key",
+      ssh_options: "StrictHostKeyChecking=no",
+      user: "ec2-user",
+      host: [
+        {
+          host: "3.20.183.221",
+          port: "4578",
+        },
+      ],
+      ref: "prod",
+      repo: "git@github.com:SkyTradeLinks/address-claiming.git",
+      path: "/home/ec2-user/pm2-deploy/address-claiming",
+      "post-deploy":
+      "bash aws_ps_get_prod_env.sh && npm install && npm run build && pm2 startOrReload ecosystem.config.js --env prod",
+      "pre-setup": "ssh-keyscan github.com >> ~/.ssh/known_hosts",
+    },
+  },
+}
