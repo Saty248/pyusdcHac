bf6888fb9a6d24be73e2492afc0fd5900f4b46fc
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-12-06 11:11:32 -0800

    hotjar installation

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,15 @@ import '@/styles/globals.css';
 import { Provider } from 'react-redux';
 import store from '@/store/store';
 import Script from 'next/script';
+import Hotjar from '@hotjar/browser';
+import { useEffect } from 'react';
 
 import { AuthProvider } from '@/hooks/useAuth';
 
 export default function App({ Component, pageProps }) {
+   useEffect(()=>{
+    Hotjar.init(process.env.NEXT_PUBLIC_HOTJAR_SITEID, process.env.NEXT_PUBLIC_HOTJAR_VERSION);
+   },[])
   return (
     <AuthProvider>
       <Provider store={store}>
