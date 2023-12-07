0cee42a0ceabc65fbbb294148ab9697d7bc54dd7
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-12-07 02:36:53 -0800

    installed hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,29 @@ import '@/styles/globals.css';
 import { Provider } from 'react-redux';
 import store from '@/store/store';
 import Script from 'next/script';
-import Hotjar from '@hotjar/browser';
-import { useEffect } from 'react';
-
 import { AuthProvider } from '@/hooks/useAuth';
 
 export default function App({ Component, pageProps }) {
-   useEffect(()=>{
-    Hotjar.init(process.env.NEXT_PUBLIC_HOTJAR_SITEID, process.env.NEXT_PUBLIC_HOTJAR_VERSION);
-   },[])
   return (
     <AuthProvider>
       <Provider store={store}>
         <>
           <Script src='https://cdn.withpersona.com/dist/persona-v4.8.0.js' />
+          <Script
+              id="show-banner"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:${Number(process.env.NEXT_PUBLIC_SITEID)},hjsv:${Number(process.env.NEXT_PUBLIC_HOTJARVERSION)}};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                `,
+              }}
+          />
           <Component {...pageProps} />
         </>
       </Provider>
