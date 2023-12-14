c6f80b828270e5b853e5f005f0dc31823324edac
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-12-14 11:37:29 -0800

    create hotjarConfig

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import store from '@/store/store';
 import Script from 'next/script';
 import { AuthProvider } from '@/hooks/useAuth';
+import { hotjarConfig } from '@/hooks/hotjarConfig';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,18 +13,7 @@ export default function App({ Component, pageProps }) {
           <Script src='https://cdn.withpersona.com/dist/persona-v4.8.0.js' />
           <Script
               id="show-banner"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  (function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:${process.env.NEXT_PUBLIC_SITEID},hjsv:${process.env.NEXT_PUBLIC_HOTJARVERSION}};
-                    a=o.getElementsByTagName('head')[0];
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-                `,
-              }}
+              dangerouslySetInnerHTML={hotjarConfig}
           />
           <Component {...pageProps} />
         </>
