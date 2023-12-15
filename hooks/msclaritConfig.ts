b79a04b79ee00dbb070ff5d3c71e1bf445d57e66
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-12-15 12:11:30 -0800

    cleaned dependencies

--- hooks/msclaritConfig.ts
+++ hooks/msclaritConfig.ts
@@ -4,6 +4,6 @@ export const msclaritConfig = {
       c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
       t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
       y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-  })(window, document, "clarity", "script", "k49q83w4ww");
+  })(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_MS_CLARIT});
     `,
   }
