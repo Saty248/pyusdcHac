cab333c436ebb519dcc4ea0553e492bff4df1c3a
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-12-19 09:56:16 -0800

    update cookie consent text

--- Components/CookieConsent.js
+++ Components/CookieConsent.js
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import { hasCookie, setCookie } from "cookies-next";
 
@@ -20,10 +22,10 @@ const CookieConsent = (props) => {
   return (
     <div className="fixed inset-0 bg-slate-700 bg-opacity-70">
       <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between px-4 py-8 bg-gray-100">
-        <span className="text-dark text-base mr-16">
-          This website uses cookies to improve user experience. By using our website you consent to all cookies in accordance with our Cookie Policy.
+        <span className="text-md text-base mr-16">
+        Welcome to SkyTrade! Like many websites, we use cookies to enhance your browsing experience, analyze site traffic, and personalize content. By clicking "Accept" you agree to the storing of cookies on your device. You can manage your preferences or withdraw your consent at any time by accessing our Cookie Settings.
         </span>
-        <button className="bg-green-400 py-2 px-8 rounded text-white" onClick={() => acceptCookie()}>
+        <button className="bg-blue-400 py-2 px-8 rounded text-white" onClick={() => acceptCookie()}>
           Accept
         </button>
       </div>
