0747b8dca7f007f102a47bdd2fba6708315cd7a0
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-12-19 01:25:22 -0800

    add cookie consent

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import { Provider } from 'react-redux';
 import store from '@/store/store';
 import Script from 'next/script';
+import CookieConsent from '@/Components/CookieConsent'
 
 import { AuthProvider } from '@/hooks/useAuth';
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }) {
         <>
           <Script src='https://cdn.withpersona.com/dist/persona-v4.8.0.js' />
           <Component {...pageProps} />
+          <CookieConsent /> 
         </>
       </Provider>
     </AuthProvider>
