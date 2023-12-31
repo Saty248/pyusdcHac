a9a266a90b61fec6e866edaa9ef7ecdd4bf4b033
miguelleonmarti <miguelleonmarti@gmail.com>
2023-12-31 14:05:44 +0000

    feature: useMobile hook

---
+++ hooks/useMobile.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+export const useMobile = () => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const isMobile = windowWidth < 768;
+
+    const handleResize = () => setWindowWidth(window.innerWidth);
+
+    useEffect(() => {
+        if (!window) return;
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    
+    return {isMobile};
+}
+
