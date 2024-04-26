c4f25dff44669273c9eb54a9ab09f2ee525feeb2
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-04-26 08:08:05 +0100

    fix bugs

--- pages/auth/join/index.js
+++ pages/auth/join/index.js
@@ -171,7 +171,6 @@ const Signup = () => {
         // dispatch(counterActions.userAuth(user));
         // localStorage.setItem('user', JSON.stringify(user));
         router.push("/homepage/dashboard2");
-        localStorage.set('new', true)
         return user;
       }
 
