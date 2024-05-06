733e0be8c05848c7ed5dcaebccdfa52bd1524475
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-05-06 08:26:53 +0100

    Centered loading icon on claim button

--- pages/homepage/airspace2/index.js
+++ pages/homepage/airspace2/index.js
@@ -682,7 +682,8 @@ const ClaimModal = ({
               className=" w-[75%] md:w-[25%] rounded-[5px] py-[10px] px-[22px] text-white bg-[#0653EA] cursor-pointer "
             >
               {claimButtonLoading ? (
-                <svg
+               <div className="flex justify-center items-center">
+                 <svg
                   className="animate-spin -ml-1 h-5 w-5 text-white"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -702,6 +703,7 @@ const ClaimModal = ({
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                   ></path>
                 </svg>
+               </div>
               ) : (
                 "Claim Airspace"
               )}
