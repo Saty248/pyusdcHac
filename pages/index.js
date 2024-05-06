fe90e04daccf82dd91ff83239cc658d163dac56e
Peter Munachi <alimunachipeter@gmail.com>
2024-05-06 14:30:34 +0100

    fix: bug fixes for white page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
-import Head from 'next/head';
-import { Fragment, useEffect } from 'react';
+import { useEffect, useContext } from 'react';
 import { useRouter } from 'next/router';
-import Script from 'next/script';
 import Spinner from '@/Components/Spinner';
+import useAutoLogout from '@/hooks/useAutoLogout';
+import { Web3authContext } from '@/providers/web3authProvider';
+import useAuth from '@/hooks/useAuth';
 
 
 export default function Home() {
+  useAutoLogout();
   const router = useRouter();
 
-  useEffect(() => {
-    const fetchedToken = JSON.parse(localStorage.getItem('openlogin_store'));
+  const { web3auth } = useContext(Web3authContext);
+  const { user, web3authStatus } = useAuth();
 
-    if (fetchedToken?.sessionId) {
-      router.push('/homepage/dashboard2');
-      return;
-    } else {
-      router.push("/auth/join");
+  useEffect(() => {
+    if (web3auth && web3auth.status === "ready") {
+      if (user?.blockchainAddress && web3authStatus) {
+        router.push('/homepage/dashboard2');
+      } else {
+        sessionStorage.clear();
+        localStorage.clear();
+        router.push('/auth/join')
+      }
     }
-  }, []);
+  }, [web3auth?.status]);
 
   return <Spinner />
 }
