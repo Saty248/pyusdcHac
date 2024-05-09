a372e44e714e91312113cf932a9b96151b7264ab
Ikenna U <iykethe1st@gmail.com>
2024-05-09 17:11:23 +0100

    Reverted _app and _document to staging instance

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import store from "@/store/store";
-// import Script from "next/script";
+import Script from "next/script";
 
 import CookieConsent from "@/Components/CookieConsent";
 
 import { msclaritConfig } from "@/hooks/msclaritConfig";
 import { useMobile } from "@/hooks/useMobile";
+import { useEffect, useState } from "react";
 import { SidebarProvider } from "@/hooks/sidebarContext";
 import { Web3authProvider } from "@/providers/web3authProvider";
 import { ToastContainer } from "react-toastify";
@@ -18,14 +18,12 @@ import "react-toastify/dist/ReactToastify.css";
 // import { persistStore } from "redux-persist";
 // import store from "@/redux/store";
 import ErrorBoundary from "@/Components/ErrorBoundary";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   const { isMobile } = useMobile();
   const [doItAgain, setDoItAgain] = useState(false);
 
   // const persistor = persistStore(store);
-
 
   useEffect(() => {
     var Tawk_API = global?.Tawk_API || undefined;
@@ -46,25 +44,34 @@ export default function App({ Component, pageProps }) {
     }
   }, [isMobile, global.Tawk_API, doItAgain]);
 
-
-
   return (
     <>
       <Provider store={store}>
-      
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <Web3authProvider>         
+        {/* <PersistGate loading={null} persistor={persistor}> */}
+        <>
+          <Script src="https://cdn.withpersona.com/dist/persona-v4.8.0.js" />
+          <Script id="show-banner" dangerouslySetInnerHTML={msclaritConfig} />
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-C0J4J56QW5" />
+          <Script id="google-analytics">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+        
+                gtag('config', 'G-C0J4J56QW5');
+            `}
+          </Script>
+          <Web3authProvider>
             <SidebarProvider>
               <ToastContainer style={{ width: "500px" }} />
               <ErrorBoundary>
-              <Component {...pageProps} />
+                <Component {...pageProps} />
               </ErrorBoundary>
             </SidebarProvider>
             <CookieConsent />
-          
-
-        </Web3authProvider>
-      {/* </PersistGate> */}
+          </Web3authProvider>
+        </>
+        {/* </PersistGate> */}
       </Provider>
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,27 @@
-import { msclaritConfig } from '@/hooks/msclaritConfig';
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from "next/script";
-
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+      
+    <link
+      href="https://api.mapbox.com/mapbox-gl-js/v3.3.0/mapbox-gl.css"
+      rel="stylesheet"
+    />
         <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
         <link
           href='https://unpkg.com/maplibre-gl@3.1.0/dist/maplibre-gl.css'
           rel='stylesheet'
         />
-         <script
+      </Head>
+      <body>
+        <div id='backdrop-root'></div>
+        <div id='modal-root'></div>
+        <Main />
+        <NextScript />
+        <script src="https://api.mapbox.com/mapbox-gl-js/v3.3.0/mapbox-gl.js"></script>
+        <script
           dangerouslySetInnerHTML={{
             __html: `
                 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
@@ -27,26 +36,6 @@ export default function Document() {
               `,
           }}
         />
-
-<Script src="https://cdn.withpersona.com/dist/persona-v4.8.0.js" />
-            <Script id="show-banner" dangerouslySetInnerHTML={msclaritConfig} />
-            <Script src="https://www.googletagmanager.com/gtag/js?id=G-C0J4J56QW5" />
-            <Script id="google-analytics">
-              {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-          
-                  gtag('config', 'G-C0J4J56QW5');
-              `}
-            </Script>
-      </Head>
-      <body>
-        <div id='backdrop-root'></div>
-        <div id='modal-root'></div>
-        <Main />
-        <NextScript />
-       
       </body>
     </Html>
   );
