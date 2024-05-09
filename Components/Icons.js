141459d63f32d39d59541726f1b7f42c1275cfc2
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-05-09 15:13:25 +0100

    wip

--- Components/Icons.js
+++ Components/Icons.js
@@ -96,6 +96,15 @@ export const LogoutIcon = ({ isActive }) => {
         </svg>
     )
 }
+export const MenuIcon = ({ isActive }) => {
+    return (
+        <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.32129 7.86621H19.3213" stroke="#5D7285" stroke-width="2" stroke-linecap="round"/>
+            <path d="M5.32129 12.8662H19.3213" stroke="#5D7285" stroke-width="2" stroke-linecap="round"/>
+            <path d="M5.32129 17.8662H19.3213" stroke="#5D7285" stroke-width="2" stroke-linecap="round"/>
+        </svg>
+    )
+}
 
 export const ArrowCompressIcon = ({ isActive }) => {
     return (
