a16ca834aeb5f3689936331d9b250a60db6297b1
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-05-15 03:40:37 +0100

    fix the sky point

--- pages/homepage/referral/index.js
+++ pages/homepage/referral/index.js
@@ -440,20 +440,31 @@ const Switcher = ({ sections, activeSection, setActiveSection }) => {
   );
 };
 
-const SkyPointBalance = () => {
+const SkyPointBalance = ({ registeredFriends, user }) => {
+  const [hasUsedReferralCode, setHasUsedReferralCode] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const userData = localStorage.getItem('user');
+      const userParsed = userData ? JSON.parse(userData) : {};
+      setHasUsedReferralCode(userParsed?.usedReferralCodeId);
+    }
+  }, []);
+
+  const skyPoints = hasUsedReferralCode ? 50 : 50 * registeredFriends;
+
   return (
     <div className="w-full md:w-[35%] px-4 md:px-[44px]">
-    <div className="py-5 px-4 md:px-[15px] rounded-[30px] bg-white gap-4 md:gap-[15px] w-full shadow-xl" style={{ boxShadow: "0px 12px 34px -10px #3A4DE926" }}>
+      <div className="py-5 px-4 md:px-[15px] rounded-[30px] bg-white gap-4 md:gap-[15px] w-full shadow-xl" style={{ boxShadow: "0px 12px 34px -10px #3A4DE926" }}>
         <div className="flex items-end justify-end">
-            <div className="w-12 h-12 md:w-[44px] md:h-[42px] rounded-full bg-[#E9F5FE] flex items-center justify-center">
-                <div className="w-6 h-6"><GiftIcon isActive={true} /></div>
-            </div>
+          <div className="w-12 h-12 md:w-[44px] md:h-[42px] rounded-full bg-[#E9F5FE] flex items-center justify-center">
+            <div className="w-6 h-6"><GiftIcon isActive={true} /></div>
+          </div>
         </div>
         <div className="text-[32px] md:text-2xl font-semibold">SKY Points Balance</div>
-        <div className="text-blue-500 font-semibold text-2xl md:text-4xl my-2 md:my-5">50 SKY Points</div>
+        <div className="text-blue-500 font-semibold text-2xl md:text-4xl my-2 md:my-5">{skyPoints} SKY Points</div>
+      </div>
     </div>
-</div>
-
   );
 };
 
@@ -505,7 +516,7 @@ const Referral = () => {
               setActiveSection={setActiveSection}
             />
             <AlertMessage />
-            <SkyPointBalance />
+            <SkyPointBalance  registeredFriends={data?.registeredFriends} user={user}/>
             <TheProgram
               activeSection={activeSection}
               isMobile={isMobile}
