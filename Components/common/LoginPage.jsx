fd417a9a38906ec9474f6150b1241d569508662d
Saty248 <mondal.sayantan1234@gmail.com>
2024-05-23 02:39:00 +0530

    update:added chagnes suggested into sidebar and page header

--- Components/common/LoginPage.jsx
+++ Components/common/LoginPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div className='fixed  w-screen h-full z-40 flex items-center justify-center z-50'>
+    <div className='fixed  w-screen h-full  flex items-center justify-center '>
       <div className='w-[25%] h-[85%]  flex justify-center items-center rounded-xl overflow-clip bg-white'>
         <Signup/>
       </div>
