84e7ed41f5e8c07ef290107014d017c34afed22a
Peter Munachi <alimunachipeter@gmail.com>
2024-06-04 15:13:37 +0100

    refactor: referral page refactor

--- Components/Referral/ReferralProgramOverview/Item.tsx
+++ Components/Referral/ReferralProgramOverview/Item.tsx
@@ -7,10 +7,12 @@ interface ItemProps {
 const Item:React.FC<ItemProps> = ({ icon, title, text }) => {
     return (
       <div
-        className="py-5 px-[15px] rounded-[30px] bg-white flex flex-col gap-[15px] items-center md:min-w-[225px] md:h-[223px] w-full shadow-[0_12px_34px_-10px_rgba(58,77,233,0.15)]"
+        className="py-5 px-[15px] rounded-[30px] bg-white flex flex-col gap-[15px] items-center md:min-w-[225px] md:h-[223px] w-full"
+        style={{ boxShadow: "0px 12px 34px -10px #3A4DE926" }}
       >
         <div
-          className="w-[63px] h-[63px] bg-[#E9F5FE] flex items-center justify-center rounded-[50%]"
+          className="w-[63px] h-[63px] bg-[#E9F5FE] flex items-center justify-center"
+          style={{ borderRadius: "50%" }}
         >
           <div className="w-9 h-9 flex items-center justify-center">{icon}</div>
         </div>
--- Components/Referral/Switcher.tsx
+++ Components/Referral/Switcher.tsx
@@ -3,20 +3,21 @@ interface SwitcherProps {
   activeSection:number;
   setActiveSection:React.Dispatch<React.SetStateAction<number>>;
 }
+
 const Switcher:React.FC<SwitcherProps> = ({ sections, activeSection, setActiveSection }) => {
-    return (
-      <div className="md:hidden flex items-center gap-[14px] mx-auto">
-        {sections.map((text, index) => (
-          <div
-            key={text}
-            onClick={() => setActiveSection(index)}
-            className={`${index === activeSection ? "bg-[#222222] text-white" : "bg-[#2222221A] text-[#222222]"} cursor-pointer text-[15px] font-normal p-[10px] rounded-[30px]`}
-          >
-            {text}
-          </div>
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div className="md:hidden flex items-center gap-[14px] mx-auto">
+      {sections.map((text, index) => (
+        <div
+          key={text}
+          onClick={() => setActiveSection(index)}
+          className={`${index === activeSection ? "bg-[#222222] text-white" : "bg-[#2222221A] text-[#222222]"} cursor-pointer text-[15px] font-normal p-[10px] rounded-[30px]`}
+        >
+          {text}
+        </div>
+      ))}
+    </div>
+  );
+};
   
 export default Switcher;
