0b1e00acd7254be07d50f30e222eededa32e38c7
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-06-07 04:53:38 +0100

    Fix Withdraw button UI on the funds page in staging

--- Components/Funds/index.tsx
+++ Components/Funds/index.tsx
@@ -21,6 +21,7 @@ import {
   KeyI,
 } from "../../types";
 import Sidebar from "../Shared/Sidebar";
+import { useMobile } from "@/hooks/useMobile";
 
 const Funds = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -33,7 +34,7 @@ const Funds = () => {
   const router = useRouter();
   const [solbalance, setSolBalance] = useState<number>(0);
   const { provider } = useContext(Web3authContext) as Web3authContextType;
-
+  const { isMobile } = useMobile();
   useEffect(() => {
     let fetchbalance = async () => {
       if (user && provider) {
@@ -121,23 +122,23 @@ const Funds = () => {
         <div className="w-full h-full flex flex-col">
           <PageHeader pageTitle={"Funds"} />
           <section className="relative  w-full h-full py-6 md:py-[37px]  flex flex-col gap-8 mb-[78.22px]  md:mb-0 overflow-y-scroll sm:pl-[68.82px] sm:pr-[55px]">
-            <div className="flex  sm:gap-[50px] flex-wrap justify-center">
-              <div className="flex flex-col gap-5 items-center sm:items-start ">
-                <AvailableBalance solbalance={solbalance} />
-                <DepositAndWithdraw
-                  walletId={user?.blockchainAddress || ""}
-                  activeSection={activeSection}
-                  setActiveSection={setActiveSection}
-                  setIsLoading={setIsLoading}
-                  isLoading={isLoading}
-                  setreFetchBal={setreFetchBal}
-                  refetchBal={refetchBal}
-                  setTokenBalance={setTokenBalance}
-                  tokenBalance={tokenBalance}
-                  solbalance={solbalance}
-                />
-              </div>
-              <TransactionHistory transactions={transactions} user={user} />
+            <div className="flex sm:gap-[50px] flex-wrap justify-center">
+            <div className={`${isMobile ? "w-full flex flex-col gap-5 items-center sm:items-start" : "flex flex-col gap-5 items-center sm:items-start"}`}>
+                  <AvailableBalance solbalance={solbalance} />
+                  <DepositAndWithdraw
+                    walletId={user?.blockchainAddress || ""}
+                    activeSection={activeSection}
+                    setActiveSection={setActiveSection}
+                    setIsLoading={setIsLoading}
+                    isLoading={isLoading}
+                    setreFetchBal={setreFetchBal}
+                    refetchBal={refetchBal}
+                    setTokenBalance={setTokenBalance}
+                    tokenBalance={tokenBalance}
+                    solbalance={solbalance}
+                  />
+                </div>
+             <TransactionHistory transactions={transactions} user={user} />
             </div>
           </section>
         </div>
