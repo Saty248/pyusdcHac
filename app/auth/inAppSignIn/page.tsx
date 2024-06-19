4bfdd36c2eeb37d21fcab132243610be9c27b1ec
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-06-19 17:16:12 +0100

    add background color for unlogged users on the login screen

--- app/auth/inAppSignIn/page.tsx
+++ app/auth/inAppSignIn/page.tsx
@@ -15,14 +15,8 @@ const InAppSignIn = () => {
         <Head>
           <title>SkyTrade </title>
         </Head>
-
-        <div className="relative rounded bg-[#F6FAFF] h-screen w-screen flex items-center justify-center overflow-hidden">
-          {!isMobile && <Sidebar />}
-          <div className={`w-full h-full flex flex-col overflow-scroll md:overflow-hidden pointer-events-none`}>
-            <section className=" md:flex relative w-full h-full md:pl-[53px] " style={{ backgroundImage: "url('/images/map-bg.png')",backgroundSize: "cover"}}
-            ></section>
-          </div>
-          {<LoginPage />}
+        <div className="bg-[#F0F0FA] h-screen w-screen ">
+        <LoginPage />
         </div>
       </Fragment>
     );
