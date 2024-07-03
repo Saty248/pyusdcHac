18ce5c00e0c2fb0254f8d59a4d32343ee155f716
emnet yohannes <emnet.yohan@gmail.com>
2024-07-03 09:47:51 +0300

    error display fixed and phone country select added as the design

--- app/auth/join/page.tsx
+++ app/auth/join/page.tsx
@@ -15,7 +15,8 @@ import PartOne from "@/Components/Auth/PartOne";
 import { useAppDispatch, useAppSelector } from "@/redux/store";
 import { setCategory } from "@/redux/slices/userSlice";
 import { toast } from "react-toastify";
-
+import { PhoneInput } from "react-international-phone";
+import "react-international-phone/style.css";
 interface RootState {
   value: {
     category: any;
@@ -41,15 +42,16 @@ const IndividualSignup: React.FC = () => {
   const referralCodeRef = useRef<HTMLInputElement>(null);
 
   const [referralCode1, setReferralCode] = useState({ id: "", code: "" });
-  const [status, setStatus] = useState(0);
+  const [status, setStatus] = useState(null);
   const [isNameValid, setIsNameValid] = useState(true);
   const [isPhoneNumberValid, setIsPhoneNumberValid] = useState(true);
+  const [isStatusValid, setIsStatusValid] = useState(true);
   const [isReferralCodeValid, setIsReferralCodeValid] = useState(true);
   const [newsletter, setNewsletter] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [pageLoad, setPageLoad] = useState(true);
   const [referralDisabled, setReferralDisabled] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("This field is mandatory");
 
   useEffect(() => {
     const categoryData = localStorage.getItem("category");
@@ -91,28 +93,31 @@ const IndividualSignup: React.FC = () => {
 
       if (name === "") {
         setIsNameValid(false);
-        return;
       }
 
       const phoneCheck = await checkPhoneIsValid(phoneNumber);
       if (!phoneCheck.status) {
         setIsPhoneNumberValid(false);
         setErrorMessage(phoneCheck.message);
-        return;
       }
 
       if (!checkReferralCodeIsValid(referralCode1)) {
         setIsReferralCodeValid(false);
-        return;
-      }
-
-      if (!checkReferralCodeIsValid(referralCode1)) {
-        setIsReferralCodeValid(false);
-        return;
       }
 
       if (!isEmailValid(category.email)) {
         toast.error("Login: email is not valid");
+      }
+      if (status === null) {
+        setIsStatusValid(false);
+      }
+      if (
+        name === "" ||
+        !phoneCheck.status ||
+        status === null ||
+        !checkReferralCodeIsValid(referralCode1) ||
+        !isEmailValid(category.email)
+      ) {
         return;
       }
 
@@ -221,21 +226,24 @@ const IndividualSignup: React.FC = () => {
                 >
                   Phone<span className="text-[#E04F64]">*</span>
                 </label>
-                <input
-                  type="text"
-                  value={phoneNumber}
-                  onChange={(e) => {
-                    setIsPhoneNumberValid(true);
-                    setPhoneNumber(e.target.value);
-                  }}
-                  placeholder="Enter your phone number"
+                <div
                   className="rounded-lg font-sans placeholder:font-medium placeholder:text-[#B8B8B8] placeholder:text-sm py-4 px-[22px] focus:outline-none"
                   style={{
                     border: isPhoneNumberValid
                       ? "1px solid #87878D"
                       : "1px solid #E04F64",
                   }}
-                />
+                >
+                  <PhoneInput
+                    defaultCountry="us"
+                    value={phoneNumber}
+                    onChange={(phone) => {
+                      setIsPhoneNumberValid(true);
+                      setPhoneNumber(phone);
+                    }}
+                    placeholder="Enter your phone number"
+                  />
+                </div>
                 {!isPhoneNumberValid && (
                   <p className="text-[11px] italic text-red-600">
                     {errorMessage}
@@ -245,20 +253,29 @@ const IndividualSignup: React.FC = () => {
               <div className="relative flex flex-col gap-[5px] w-full">
                 <label
                   className="text-[14px] font-normal"
-                  style={{ color: "rgba(0, 0, 0, 0.50)" }}
+                  style={{
+                    color: isStatusValid ? "rgba(0, 0, 0, 0.50)" : "#E04F64",
+                  }}
                 >
                   Your status<span className="text-[#E04F64]">*</span>
                 </label>
                 <div className="flex flex-col gap-[11px]">
                   <label
                     className="rounded-lg py-4 px-[22px] flex gap-[14.5px] items-center text-[14px]"
-                    style={{ border: "1px solid #87878D" }}
+                    style={{
+                      border: isStatusValid
+                        ? "1px solid #87878D"
+                        : "1px solid #E04F64",
+                    }}
                   >
                     <input
                       className="relative w-[16.67px] h-[16.67px] p-[2.5px]"
                       checked={status === 0}
                       value={0}
-                      onChange={(e) => setStatus(Number(e.target.value))}
+                      onChange={(e) => {
+                        setIsStatusValid(true);
+                        setStatus(Number(e.target.value));
+                      }}
                       style={{
                         appearance: "none",
                         border:
@@ -284,7 +301,10 @@ const IndividualSignup: React.FC = () => {
                       className="relative w-[16.67px] h-[16.67px] p-[2.5px]"
                       checked={status === 1}
                       value={1}
-                      onChange={(e) => setStatus(Number(e.target.value))}
+                      onChange={(e) => {
+                        setIsStatusValid(true);
+                        setStatus(Number(e.target.value));
+                      }}
                       style={{
                         appearance: "none",
                         border:
@@ -303,7 +323,7 @@ const IndividualSignup: React.FC = () => {
                     I'm a corporate entity
                   </label>
                 </div>
-                {false && (
+                {!isStatusValid && (
                   <p className="text-[11px] italic text-red-600">
                     This field is mandatory
                   </p>
