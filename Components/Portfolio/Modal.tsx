d31466f9330a508e9fcf397dc0502ab745322b45
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-08-20 16:52:58 +0100

    Added functionality to Edit button for Claimed Airspace

--- Components/Portfolio/Modal.tsx
+++ Components/Portfolio/Modal.tsx
@@ -1,16 +1,46 @@
+import useAuth from "@/hooks/useAuth";
+import AirspaceRentalService from "@/services/AirspaceRentalService";
+import PropertiesService from "@/services/PropertiesService";
+import { PropertyData } from "@/types";
 import { formatDate } from "@/utils";
+import { SetStateAction, useState } from "react";
 
 const { Fragment } = require("react");
 const { ArrowLeftIcon, CloseIcon, LocationPointIcon } = require("../Icons");
-
 interface ModalProps {
-    airspace:any;
+  airspace: any
     onCloseModal:() => void ;
     isOffer?:boolean
+    pageNumber?: number
+    setAirspaceList:  (value: SetStateAction<PropertyData[]>) => void
 }
 
 
-const  Modal = ({ airspace, onCloseModal, isOffer }: ModalProps) => {
+const  Modal = ({ airspace, onCloseModal, isOffer, pageNumber = 0, setAirspaceList }: ModalProps) => {
+  const [inputValue, setInputValue] = useState(airspace?.address);
+  const {editAirSpaceAddress} = PropertiesService()
+  const [isLoading, setIsEditLoading] = useState(false)
+  const { user } = useAuth();
+  const {getUnverifiedAirspaces} = AirspaceRentalService();
+
+  console.log(user, "user")
+const handleEdit = async () => {
+  if (!user || inputValue === airspace?.address) return;
+  setIsEditLoading(true);
+  const editResponse = await editAirSpaceAddress({ address: inputValue, propertyId: airspace.id });
+  console.log(editResponse, "editResponse");
+
+  console.log(airspace, "airspace");
+  const airspaceResp = await getUnverifiedAirspaces(
+    user?.blockchainAddress,
+    pageNumber,
+    10,
+  );
+  console.log(airspaceResp, "airspaceResp")
+  setAirspaceList(airspaceResp.items);
+  setIsEditLoading(false);
+};
+
   return (
     <Fragment>
       <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white py-[30px] md:rounded-[30px] px-[29px] w-full h-full md:h-auto md:w-[689px] z-[500] md:z-50 flex flex-col gap-[15px]">
@@ -21,9 +51,8 @@ const  Modal = ({ airspace, onCloseModal, isOffer }: ModalProps) => {
           <div className="w-[16px] h-[12px] md:hidden" onClick={onCloseModal}>
             <ArrowLeftIcon />
           </div>
-          <h2 className="text-[#222222] text-center font-medium text-xl break-words">
-           
-          {airspace?.address.length > 60 ?  airspace?.address.slice(0, 57) + ' ...' : airspace?.address}
+          <h2 className="text-light-black text-center font-medium text-xl">
+            {inputValue}
           </h2>
           <div
             onClick={onCloseModal}
@@ -32,32 +61,33 @@ const  Modal = ({ airspace, onCloseModal, isOffer }: ModalProps) => {
             <CloseIcon />
           </div>
         </div>
+       
         <div
-          className="flex items-center gap-[10px] py-4 px-[22px] rounded-lg"
-          style={{ border: "1px solid #4285F4" }}
+          className="flex items-center gap-[10px] py-4 px-[22px] rounded-lg border border-deep-blue"
         >
           <div className="w-6 h-6">
             <LocationPointIcon />
           </div>
-          <p className="font-normal text-[#222222] text-[14px] flex-1 break-words">
-            
-            {airspace?.address}
-          </p>
-        </div>
-
+          <input className="font-normal text-light-black text-[14px] flex-1 border-none outline-none" 
+          type="text"
+          value={inputValue}
+          onChange={ (e) => { setInputValue(e.target.value); }}
+          />
+     </div>
+ 
         <div className="flex gap-[15px]">
-          <p className="text-[14px] font-normal text-[#222222]">ID:</p>
-          <p className="text-[14px] font-normal text-[#87878D] break-all">
+          <p className="text-[14px] font-normal text-light-black">ID:</p>
+          <p className="text-[14px] font-normal text-light-grey break-all">
             {airspace?.id}
           </p>
         </div>
 
         {airspace?.metadata?.endTime && (
           <div className="flex gap-[15px]">
-            <p className="text-[14px] font-normal text-[#222222]">
+            <p className="text-[14px] font-normal text-light-black">
               Expiration Date:
             </p>
-            <p className="text-[14px] font-normal text-[#87878D]">
+            <p className="text-[14px] font-normal text-light-grey">
               {formatDate(airspace?.metadata?.endTime)}
             </p>
           </div>
@@ -72,7 +102,7 @@ const  Modal = ({ airspace, onCloseModal, isOffer }: ModalProps) => {
               <p className="font-normal text-[12px] text-[#838187]">
                 Offer received
               </p>
-              <p className="font-bold text-2xl text-[#222222]">
+              <p className="font-bold text-2xl text-light-black">
               {/*  {USDollar.format(99.87)} */}
               </p>
             </div>
@@ -100,10 +130,11 @@ const  Modal = ({ airspace, onCloseModal, isOffer }: ModalProps) => {
               Cancel
             </div>
             <button
-              disabled
-              className="disabled flex-1 text-white rounded-[5px] bg-gray-300 cursor-not-allowed text-center py-[10px] px-[20px] flex items-center justify-center"
+              // disabled
+              onClick={handleEdit}
+              className=" flex-1 text-white rounded-[5px] bg-[#0653EA]  text-center py-[10px] px-[20px] flex items-center justify-center"
             >
-              Edit
+              {isLoading ? "Editing ..." : "Edit"}
             </button>
           </div>
         )}
